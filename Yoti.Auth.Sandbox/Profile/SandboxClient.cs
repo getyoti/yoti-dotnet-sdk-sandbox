9bd4ab910e7374590efd21a21a97856007accdde
echarrod <echarrod@users.noreply.github.com>
2020-03-17 12:58:12 +0000

    SDK-1466: Expose sandbox

---
+++ Yoti.Auth.Sandbox/Profile/SandboxClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto;
+using Yoti.Auth.Sandbox.Profile.Request;
+using Yoti.Auth.Sandbox.Profile.Response;
+using Yoti.Auth.Web;
+
+namespace Yoti.Auth.Sandbox.Profile
+{
+    public class SandboxClient
+    {
+        private const string _yotiSandboxPathPrefix = "/sandbox/v1";
+        private readonly Uri _defaultSandboxApiUrl = new Uri(Constants.Api.DefaultYotiHost + _yotiSandboxPathPrefix);
+        private readonly HttpClient _httpClient;
+        private readonly Uri _apiUri;
+        private readonly string _appId;
+        private readonly AsymmetricCipherKeyPair _keyPair;
+
+        public static SandboxClientBuilder Builder()
+        {
+            return new SandboxClientBuilder();
+        }
+
+        public SandboxClient(HttpClient httpClient, Uri apiUri, string appId, AsymmetricCipherKeyPair keyPair)
+        {
+            _httpClient = httpClient;
+            _apiUri = apiUri ?? _defaultSandboxApiUrl;
+            _appId = appId;
+            _keyPair = keyPair;
+        }
+
+        public string SetupSharingProfile(YotiTokenRequest yotiTokenRequest)
+        {
+            try
+            {
+                string serializedTokenRequest = JsonConvert.SerializeObject(yotiTokenRequest);
+                byte[] body = Encoding.UTF8.GetBytes(serializedTokenRequest);
+
+                Yoti.Auth.Web.Request request = new RequestBuilder()
+                    .WithKeyPair(_keyPair)
+                    .WithBaseUri(_apiUri)
+                    .WithEndpoint($"/apps/{_appId}/tokens")
+                    .WithHttpMethod(HttpMethod.Post)
+                    .WithContent(body)
+                    .Build();
+
+                HttpResponseMessage response = request.Execute(_httpClient).Result;
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    Yoti.Auth.Web.Response.CreateExceptionFromStatusCode<SandboxException>(response);
+                }
+
+                YotiTokenResponse yotiTokenResponse =
+                    JsonConvert.DeserializeObject<YotiTokenResponse>(
+                        response.Content.ReadAsStringAsync().Result);
+
+                return yotiTokenResponse.Token;
+            }
+            catch (Exception ex)
+            {
+                throw new SandboxException(Properties.Resources.SharingProfileError, ex);
+            }
+        }
+    }
+}
---
+++ Yoti.Auth.Sandbox/Profile/SandboxClientBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using Org.BouncyCastle.Crypto;
+
+namespace Yoti.Auth.Sandbox.Profile
+{
+    public class SandboxClientBuilder
+    {
+        private string _appId;
+        private AsymmetricCipherKeyPair _keyPair;
+        private Uri _apiUri;
+
+        public SandboxClientBuilder()
+        {
+        }
+
+        public SandboxClientBuilder WithApiUri(Uri apiUri)
+        {
+            _apiUri = apiUri;
+
+            return this;
+        }
+
+        public SandboxClientBuilder ForApplication(string appId)
+        {
+            _appId = appId;
+            return this;
+        }
+
+        public SandboxClientBuilder WithKeyPair(AsymmetricCipherKeyPair keypair)
+        {
+            _keyPair = keypair;
+            return this;
+        }
+
+        public SandboxClient Build()
+        {
+            Validation.NotNull(_appId, nameof(_appId));
+            Validation.NotNull(_keyPair, nameof(_keyPair));
+            Validation.NotNull(_apiUri, nameof(_apiUri));
+
+            return new SandboxClient(new HttpClient(), _apiUri, _appId, _keyPair);
+        }
+    }
+}
