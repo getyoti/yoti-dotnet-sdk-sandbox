f0c50db269589a4426f5407cbc75575ae8ee5122
echarrod <echarrod@users.noreply.github.com>
2020-10-06 17:08:02 +0100

    SDK-1719: Add WithCountryCode & WithDocumentType (singular)

--- Yoti.Auth.Sandbox.Tests/DocScan/Request/Task/SandboxDocumentFilterBuilderTests.cs
+++ Yoti.Auth.Sandbox.Tests/DocScan/Request/Task/SandboxDocumentFilterBuilderTests.cs
@@ -1,28 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Xunit;
-using Yoti.Auth.Sandbox.DocScan.Request.Check;
 using Yoti.Auth.Sandbox.DocScan.Request.Task;
 
 namespace Yoti.Auth.Sandbox.Tests.DocScan.Request.Task
 {
     public class SandboxDocumentFilterBuilderTests
     {
-        private readonly List<string> _someCountryCodes = new List<string> { "CR1", "CR2" };
-        private readonly List<string> _someDocumentTypes = new List<string> { "DOCUMENT_TYPE_1", "DOCUMENT_TYPE_2" };
+        private readonly string _country1 = "CR1";
+        private readonly string _country2 = "CR2";
+        private readonly string _documentType1 = "DOCUMENT_TYPE_1";
+        private readonly string _documentType2 = "DOCUMENT_TYPE_2";
 
         [Fact]
-        public void ShouldBuildSandboxDocumentFilter()
+        public void ShouldBuildSandboxDocumentFilterWithCountryCode()
         {
             var filter = new SandboxDocumentFilterBuilder()
-                .WithCountryCodes(_someCountryCodes)
-                .WithDocumentTypes(_someDocumentTypes)
+                .WithCountryCode(_country1)
                 .Build();
 
-            Assert.Equal(_someCountryCodes, filter.CountryCodes);
-            Assert.Equal(_someDocumentTypes, filter.DocumentTypes);
+            Assert.Equal(_country1, filter.CountryCodes.Single());
+        }
+
+        [Fact]
+        public void ShouldBuildSandboxDocumentFilteMultipleWithCountryCodes()
+        {
+            var filter = new SandboxDocumentFilterBuilder()
+                .WithCountryCode(_country1)
+                .WithCountryCode(_country2)
+                .Build();
+
+            Assert.Equal(new List<string> { _country1, _country2 }, filter.CountryCodes);
+        }
+
+        [Fact]
+        public void ShouldBuildSandboxDocumentFilterWithCountryCodes()
+        {
+            var countryCodes = new List<string> { _country1, _country2 };
+
+            var filter = new SandboxDocumentFilterBuilder()
+                .WithCountryCodes(countryCodes)
+                .Build();
+
+            Assert.Equal(countryCodes, filter.CountryCodes);
+        }
+
+        [Fact]
+        public void ShouldBuildSandboxDocumentFilterWithDocumentType()
+        {
+            var filter = new SandboxDocumentFilterBuilder()
+                .WithDocumentType(_country1)
+                .Build();
+
+            Assert.Equal(_country1, filter.DocumentTypes.Single());
+        }
+
+        [Fact]
+        public void ShouldBuildSandboxDocumentFilterMultipleWithDocumentTypes()
+        {
+            var filter = new SandboxDocumentFilterBuilder()
+                .WithDocumentType(_documentType1)
+                .WithDocumentType(_documentType2)
+                .Build();
+
+            Assert.Equal(new List<string> { _documentType1, _documentType2 }, filter.DocumentTypes);
+        }
+
+        [Fact]
+        public void ShouldBuildSandboxDocumentFilterWithDocumentTypes()
+        {
+            var documentTypes = new List<string> { _documentType1, _documentType2 };
+
+            var filter = new SandboxDocumentFilterBuilder()
+                .WithDocumentTypes(documentTypes)
+                .Build();
+
+            Assert.Equal(documentTypes, filter.DocumentTypes);
         }
     }
 }
