a74200ed3eb68f55cfea00db96280b784c634638
echarrod <echarrod@users.noreply.github.com>
2020-10-06 16:59:14 +0100

    SDK-1719: Add coverage for SandboxIdDocumentComparisonCheckBuilder

---
+++ Yoti.Auth.Sandbox.Tests/DocScan/Request/Check/SandboxIdDocumentComparisonCheckBuilderTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Xunit;
+using Yoti.Auth.Sandbox.DocScan.Request.Check;
+using Yoti.Auth.Sandbox.DocScan.Request.Check.Report;
+using Yoti.Auth.Sandbox.DocScan.Request.Task;
+
+namespace Yoti.Auth.Sandbox.Tests.DocScan.Request.Check
+{
+    public class SandboxIdDocumentComparisonCheckBuilderTests
+    {
+        [Fact]
+        public void ShouldBuildWithOnlyRecommendation()
+        {
+            var idDocComparisonCheck = new SandboxIdDocumentComparisonCheckBuilder()
+                .WithRecommendation(
+                    new SandboxRecommendationBuilder()
+                    .WithValue("APPROVE")
+                    .Build())
+                .Build();
+
+            Assert.Equal("APPROVE", idDocComparisonCheck.Result.Report.Recommendation.Value);
+        }
+
+        [Fact]
+        public void ShouldBuildWithSecondaryDocumentFilter()
+        {
+            var idDocComparisonCheck = new SandboxIdDocumentComparisonCheckBuilder()
+                 .WithRecommendation(
+                     new SandboxRecommendationBuilder()
+                     .WithValue("APPROVE")
+                     .Build())
+                 .WithSecondaryDocumentFilter(
+                    new SandboxDocumentFilterBuilder()
+                    .WithCountryCode("GBR")
+                    .Build())
+                 .Build();
+
+            Assert.Equal("GBR", idDocComparisonCheck.SecondaryDocumentFilter.CountryCodes.Single());
+        }
+    }
+}
