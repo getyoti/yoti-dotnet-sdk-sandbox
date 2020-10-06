3c95f8228c3da728faa111513a4ca52e09ccab36
echarrod <echarrod@users.noreply.github.com>
2020-10-06 17:08:02 +0100

    SDK-1719: Add WithCountryCode & WithDocumentType (singular)

--- Yoti.Auth.Sandbox/DocScan/Request/Task/SandboxDocumentFilterBuilder.cs
+++ Yoti.Auth.Sandbox/DocScan/Request/Task/SandboxDocumentFilterBuilder.cs
@@ -7,9 +7,31 @@ namespace Yoti.Auth.Sandbox.DocScan.Request.Task
         private List<string> _documentTypes;
         private List<string> _countryCodes;
 
+        public SandboxDocumentFilterBuilder WithDocumentType(string documentType)
+        {
+            if (_documentTypes == null)
+            {
+                _documentTypes = new List<string>();
+            }
+
+            _documentTypes.Add(documentType);
+            return this;
+        }
+
         public SandboxDocumentFilterBuilder WithDocumentTypes(List<string> documentTypes)
         {
             _documentTypes = documentTypes;
+            return this;
+        }
+
+        public SandboxDocumentFilterBuilder WithCountryCode(string countryCode)
+        {
+            if (_countryCodes == null)
+            {
+                _countryCodes = new List<string>();
+            }
+
+            _countryCodes.Add(countryCode);
             return this;
         }
 
