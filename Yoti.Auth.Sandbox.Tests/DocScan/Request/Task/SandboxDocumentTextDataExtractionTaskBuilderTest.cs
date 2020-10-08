1570406e68701b663808ad1f19a584882d6cdb9d
echarrod <echarrod@users.noreply.github.com>
2020-10-08 15:20:51 +0100

    SDK-1758: Add ability to set chip data - document ID photo

--- Yoti.Auth.Sandbox.Tests/DocScan/Request/Task/SandboxDocumentTextDataExtractionTaskBuilderTest.cs
+++ Yoti.Auth.Sandbox.Tests/DocScan/Request/Task/SandboxDocumentTextDataExtractionTaskBuilderTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using Yoti.Auth.Sandbox.DocScan.Request.Task;
+using System;
+using System.Text;
 
 namespace Yoti.Auth.Sandbox.Tests.DocScan.Request.Task
 {
